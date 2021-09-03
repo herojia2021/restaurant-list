fd0917b3e7c2e1db539cce2ef8016f87c27f1010
herojia <hodalaplus@gmail.com>
2021-09-04 03:23:00 +0800

    新增路由, 調整UI, 變更handlebars設定 1.新增/restaurants, /search 路由 2.新增餐廳資料頁面, 查詢結果頁面 3.Layout Template 微調 4.變更 .handlebars 為 .hbs

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const port = 3000
 
 const exphbs = require("express-handlebars")
 
+const restaurantData = require("./restaurant.json")
+
 // setting template engine
-app.engine("handlebars", exphbs({ defaultLayout: "main.hbs" }))
-app.set("view engine", "handlebars")
+app.engine("hbs", exphbs({ extname: "hbs", defaultLayout: "main" }))
+app.set("view engine", "hbs")
 
 app.use(express.static("public"))
 
 // setting the route and corresponding response
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index", { restaurants: restaurantData.results })
+})
+
+app.get("/restaurants/:id", (req, res) => {
+  const restaurant = restaurantData.results.find((restaurant) => restaurant.id.toString() === req.params.id)
+  res.render("show", { restaurant })
+})
+
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantData.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render("search", { restaurants, keyword })
 })
 
 // Listen the server when it started
