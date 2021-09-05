d04ebf4007958f9ce26e887c9b6d8405af5c5a68
herojia <hodalaplus@gmail.com>
2021-09-06 04:03:12 +0800

    效能議題修正, 編輯README 1. 將 toLowerCase 移出 callback, 節省非必要的呼叫 2. 新增路由註解 3. 編輯 README 4. 新增運行截圖

---
+++ README.md
@@ -0,0 +1,52 @@
+# 餐廳清單
+
+收集了各地受歡迎的餐廳
+
+## 功能列表
+
+- 查看餐廳列表
+- 查看餐廳的詳細資料
+- 以店名查詢餐廳
+- 以類別查詢餐廳
+
+### 需求
+
+- Node.js & npm
+- port 3000
+- Nodemon (optional)
+
+### 安裝
+
+1.在本地目錄 clone repo 或 [download](https://github.com/herojia2021/restaurant-list/archive/refs/heads/master.zip)
+
+```
+git clone https://github.com/herojia2021/restaurant-list.git
+```
+
+2.安裝相依套件
+
+```
+cd restaurant-list
+```
+
+```
+npm install
+```
+
+3.開啟程式
+
+```
+npm run start
+```
+
+- 終端顯示 `Express is running on http://localhost:3000` 即啟動完成，
+- 用瀏覽器開啟 [http://localhost:3000](http://localhost:3000) 使用程式
+
+## 運行截圖
+
+empty
+
+## 相依套件
+
+- [Express](https://www.npmjs.com/package/express) - Framework
+- [Express-Handlebars](https://www.npmjs.com/package/express-handlebars) - Template Engine
--- app.js
+++ app.js
@@ -12,20 +12,23 @@ app.set("view engine", "hbs")
 
 app.use(express.static("public"))
 
-// setting the route and corresponding response
+// 所有餐廳清單
 app.get("/", (req, res) => {
   res.render("index", { restaurants: restaurantData.results })
 })
 
+// 餐廳詳細資料頁面
 app.get("/restaurants/:id", (req, res) => {
   const restaurant = restaurantData.results.find((restaurant) => restaurant.id.toString() === req.params.id)
   res.render("show", { restaurant })
 })
 
+// 搜尋餐廳類別、餐廳店名
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword
+  const lowerCaseKeyword = keyword.toLowerCase()
   const restaurants = restaurantData.results.filter((restaurant) => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(lowerCaseKeyword) || restaurant.category.toLowerCase().includes(lowerCaseKeyword)
   })
   res.render("search", { restaurants, keyword })
 })
