8b721fda4a52c3a74af6757e88ece68983fcf63c
herojia <hodalaplus@gmail.com>
2021-09-17 04:19:02 +0800

    增加新增,刪除,修改餐廳功能, 移除id改使用自動產生_id 1.新增新增餐廳頁面 2.新增修改餐廳頁面 3.新增新增,檢視,修改,刪除按鈕 4.新增新增,刪除,修改路由 5.移除資料庫id欄位改用自動產生的_id 6.引入 body-parser 套件

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
 const Restaurant = require("./models/restaurant")
+const bodyParser = require("body-parser")
 
 // setup Application
 const app = express()
@@ -26,6 +27,8 @@ app.set("view engine", "hbs")
 // setup static-file path
 app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // route: 所有餐廳清單
 app.get("/", (req, res) => {
   Restaurant.find()
@@ -34,15 +37,26 @@ app.get("/", (req, res) => {
     .catch((error) => console.error(error))
 })
 
+app.get("/restaurants/new", (req, res) => {
+  return res.render("new")
+})
+
+app.post("/restaurants", (req, res) => {
+  let newRestaurant = {}
+  for (const [key, value] of Object.entries(req.body)) {
+    newRestaurant[key] = value
+  }
+  return Restaurant.create(newRestaurant)
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error))
+})
+
 // route: 餐廳詳細資料頁面
 app.get("/restaurants/:id", (req, res) => {
-  const id = Number(req.params.id)
-  return Restaurant.find({ id })
+  const id = req.params.id
+  return Restaurant.findById(id)
     .lean()
-    .then((restaurant) => {
-      console.log(restaurant[0])
-      return res.render("show", { restaurant: restaurant[0] })
-    })
+    .then((restaurant) => res.render("show", { restaurant }))
     .catch((error) => console.log(error))
 })
 
@@ -51,7 +65,36 @@ app.get("/search", (req, res) => {
   const keyword = req.query.keyword
   return Restaurant.find({ $or: [{ name: { $regex: keyword, $options: "i" } }, { category: { $regex: keyword, $options: "i" } }] })
     .lean()
-    .then((restaurants) => res.render("index", { restaurants: restaurants, keyword: keyword }))
+    .then((restaurants) => res.render("index", { restaurants, keyword }))
+    .catch((error) => console.log(error))
+})
+
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      for (const [key, value] of Object.entries(req.body)) {
+        restaurant[key] = value
+      }
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error))
+})
+
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect("/"))
     .catch((error) => console.log(error))
 })
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -15,7 +15,7 @@ db.once("open", () => {
   console.log("mongodb connected!")
 
   restaurantData.results.forEach((element) => {
-    Restaurant.create({ id: element.id, name: element.name, name_en: element.name_en, category: element.category, image: element.image, location: element.location, phone: element.phone, google_map: element.google_map, rating: element.rating, description: element.description })
+    Restaurant.create({ name: element.name, name_en: element.name_en, category: element.category, image: element.image, location: element.location, phone: element.phone, google_map: element.google_map, rating: element.rating, description: element.description })
   })
 
   console.log("done")
