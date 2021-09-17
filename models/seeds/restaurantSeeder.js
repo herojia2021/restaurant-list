8f83d87ea2e60351adfb01a3e2767ded5de35d5e
herojia <hodalaplus@gmail.com>
2021-09-17 13:57:17 +0800

    1.匯入種子資料後關閉程式 2.編輯 README

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -15,8 +15,19 @@ db.once("open", () => {
   console.log("mongodb connected!")
 
   restaurantData.results.forEach((element) => {
-    Restaurant.create({ name: element.name, name_en: element.name_en, category: element.category, image: element.image, location: element.location, phone: element.phone, google_map: element.google_map, rating: element.rating, description: element.description })
+    Restaurant.create({
+      name: element.name,
+      name_en: element.name_en,
+      category: element.category,
+      image: element.image,
+      location: element.location,
+      phone: element.phone,
+      google_map: element.google_map,
+      rating: element.rating,
+      description: element.description,
+    })
   })
 
-  console.log("done")
+  console.log("Seed data created. Exit.")
+  process.exit()
 })
