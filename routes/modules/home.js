5152fe266d8ad7059acc5189a98e364b5fb65706
herojia <hodalaplus@gmail.com>
2021-09-18 21:42:57 +0800

    新增排序選單

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,9 +5,19 @@ const Restaurant = require("../../models/restaurant")
 
 // route: 所有餐廳清單
 router.get("/", (req, res) => {
+  const sortValStr = req.query.sort //"a-z", "z-a", "category", "location", "rating"
+  const queryObj = {
+    "a-z": { name: "asc" },
+    "z-a": { name: "desc" },
+    category: { category: "asc" },
+    location: { location: "asc" },
+    rating: { rating: "desc" },
+  }
+  const sort = sortValStr ? { [sortValStr]: true } : { "a-z": true }
   Restaurant.find()
     .lean()
-    .then((restaurants) => res.render("index", { restaurants }))
+    .sort(queryObj[sortValStr]) // desc
+    .then((restaurants) => res.render("index", { restaurants, sort }))
     .catch((error) => console.error(error))
 })
 
