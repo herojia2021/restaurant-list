868db3ed613c44de503283f9336eeffcc68069f3
herojia <hodalaplus@gmail.com>
2021-10-22 07:01:29 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -50,4 +50,10 @@ router.post("/register", (req, res) => {
   })
 })
 
+// request logout
+router.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/users/login")
+})
+
 module.exports = router
