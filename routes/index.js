5de325c2469e365ea72bb209913e6ac391a60065
herojia <hodalaplus@gmail.com>
2021-10-22 07:29:31 +0800

    feat: add authentication middleware

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const { authenticator } = require("../middleware/auth") // 掛載 middleware
 
 const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
 const users = require("./modules/users")
 
-router.use("/", home)
-router.use("/restaurants", restaurants)
-router.use("/users", users)
+router.use("/restaurants", authenticator, restaurants)
+router.use("/users", users) //login, register, logout 不需驗証
+router.use("/", authenticator, home)
 
 module.exports = router
