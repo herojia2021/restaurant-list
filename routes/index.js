88f036e9ff9fd18c4d11801d6e1960785ba63bc9
herojia <hodalaplus@gmail.com>
2021-10-22 06:07:20 +0800

    feat: add register page

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
-const user = require("./modules/user")
+const users = require("./modules/users")
 
 router.use("/", home)
 router.use("/restaurants", restaurants)
-router.use("/user", user)
+router.use("/users", users)
 
 module.exports = router
