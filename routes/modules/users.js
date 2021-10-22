f252261b9f714b205ba6ddda762ee604ed35b738
herojia <hodalaplus@gmail.com>
2021-10-22 15:25:24 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const User = require("../../models/user")
 const passport = require("passport")
+const bcrypt = require("bcryptjs")
 
 // login form
 router.get("/login", (req, res) => {
@@ -56,11 +57,16 @@ router.post("/register", (req, res) => {
       })
     } else {
       // 如果還沒註冊：寫入資料庫
-      return User.create({
-        name,
-        email,
-        password,
-      })
+      return bcrypt
+        .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+        .then((salt) => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            password: hash,
+          })
+        )
         .then(() => res.redirect("/"))
         .catch((err) => console.log(err))
     }
