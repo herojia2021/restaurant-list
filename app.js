18a810661845d45fd9811b2828ca5f175e67c7d0
herojia <hodalaplus@gmail.com>
2021-10-22 12:55:40 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require("express-session")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override")
+const flash = require("connect-flash")
 
 const routes = require("./routes")
 require("./config/mongoose")
@@ -34,11 +35,14 @@ app.use(methodOverride("_method"))
 
 // 呼叫 Passport 函式並傳入 app，要在路由之前
 usePassport(app)
+app.use(flash())
 
 app.use((req, res, next) => {
   // 保留驗証結果供view使用
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash("success_msg") // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash("warning_msg") // 設定 warning_msg 訊息
   next()
 })
 
