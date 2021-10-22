735197197cd1817fd32d94c1223a0ab504607056
herojia <hodalaplus@gmail.com>
2021-10-23 01:58:23 +0800

    feat: add 404 handling

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,16 @@ const express = require("express")
 const router = express.Router()
 const { authenticator } = require("../middleware/auth") // 掛載 middleware
 
-const home = require("./modules/home")
 const restaurants = require("./modules/restaurants")
 const users = require("./modules/users")
 const auth = require("./modules/auth")
+const home = require("./modules/home")
+const error = require("./modules/error")
 
 router.use("/restaurants", authenticator, restaurants)
 router.use("/users", users) // login, register, logout 不需驗証
 router.use("/auth", auth)
 router.use("/", authenticator, home)
+router.use("*", error)
 
 module.exports = router
