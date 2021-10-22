8d313926a712add5027e32497fbe68d1ce5f0944
herojia <hodalaplus@gmail.com>
2021-10-23 01:57:43 +0800

    feat: modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,48 @@
+const bcrypt = require("bcryptjs")
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 const Restaurant = require("../restaurant")
+const User = require("../user")
 const db = require("../../config/mongoose")
 
-const restaurantData = require("./restaurant.json")
-
+const userData = require("./users.json")
+// create seed data
 db.once("open", () => {
-  restaurantData.results.forEach((element) => {
-    Restaurant.create({
-      name: element.name,
-      name_en: element.name_en,
-      category: element.category,
-      image: element.image,
-      location: element.location,
-      phone: element.phone,
-      google_map: element.google_map,
-      rating: element.rating,
-      description: element.description,
+  Promise.all(
+    userData.users.map((userObj) => {
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(userObj.password, salt))
+        .then((hash) =>
+          User.create({
+            name: userObj.name,
+            email: userObj.email,
+            password: hash,
+          })
+        )
+        .then((user) => {
+          const userId = user._id
+          return Promise.all(
+            userObj.restaurants.map((restaurantObj) => {
+              return Restaurant.create({
+                userId,
+                name: restaurantObj.name,
+                name_en: restaurantObj.name_en,
+                category: restaurantObj.category,
+                image: restaurantObj.image,
+                location: restaurantObj.location,
+                phone: restaurantObj.phone,
+                google_map: restaurantObj.google_map,
+                rating: restaurantObj.rating,
+                description: restaurantObj.description,
+              })
+            })
+          )
+        })
     })
+  ).then(() => {
+    console.log("done.")
+    process.exit()
   })
-
-  console.log("Seed data created. Ctrl+C to Exit.")
 })
